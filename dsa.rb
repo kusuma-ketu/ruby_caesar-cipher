a57e53eab05aec9855990312925a9e130e8088b8
kusuma_.__ <51140474+kusuma-ketu@users.noreply.github.com>
2024-08-05 23:22:40 -0700

    added linkedlist

---
+++ dsa.rb
@@ -0,0 +1,16 @@
+class Node: 
+   attr_accessor: :value, :next_node
+   
+   def initialize(value):
+      @value = value
+      @next_node = nil
+      end
+   end 
+
+class LinkedList:
+   attr_accessor: :head
+      @head = head
+   end
+   
+   def initialize(node):
+      head = node
